a3b4989a38374cf2370b740ad96c2e0bc3cf9dc6
anitej-singh <83867119+anitej-singh@users.noreply.github.com>
2021-05-09 16:07:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function showhouse() {
+    document.getElementById("img1").src = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Tiny_house%2C_Portland.jpg/220px-Tiny_house%2C_Portland.jpg";
+}
+
+function showprankandrealimage() {
+     document.getElementById("realhouse").src = "http://static-17.sinclairstoryline.com/resources/media/5652ba33-882b-4932-b95f-10d6ee380246-large16x9_Dunluce1FrontExterior1.jpg";
+    
+    document.getElementById("ilovepranks").innerHTML = "sorry it was a prank";
+}
